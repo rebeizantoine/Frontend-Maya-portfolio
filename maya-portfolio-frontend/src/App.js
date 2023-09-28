9ef33a5071cedff2c6a91a78676c48904868cbd2
Leslie Saliba <lesliesaliba@gmail.com>
2023-09-28 12:15:18 +0300

    Feat login page as React component

--- maya-portfolio-frontend/src/App.js
+++ maya-portfolio-frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Dashboard from './Dashboard';
+import Login from './Login';
 import './dashboard.css'; 
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      
     </div>
   );
 }
