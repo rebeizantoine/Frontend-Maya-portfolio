8045f32f66cb65548997af2d9400bf4f56c665c8
Leslie Saliba <lesliesaliba@gmail.com>
2023-10-05 13:02:07 +0300

    Feat download-cv linked to db

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,23 @@ function Hero() {
     setTimeout(type, 200); // Start typing after clearing
   }, []);
 
-  return (
+  const handleDownload = () => {
+    fetch('http://localhost:5000/api/cv') // Fetch the CV from your server
+      .then((response) => response.blob())
+      .then((data) => {
+        const url = window.URL.createObjectURL(data);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'Maya-Atiah-Cv-FullStackWebDeveloper.pdf'; // Update with your CV file name
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch((error) => {
+        console.error('Error while downloading CV:', error);
+      });
+  };
+return (
     <div className="hero">
       <div className="hero-content">
         <p className="bubble">HELLO</p>
@@ -41,7 +57,9 @@ function Hero() {
           I am <span className="span-name">{name}</span>
           <br />a Full Stack Web Developer
         </p>
-        <button className="download-cv">DOWNLOAD CV</button>
+        <button className="download-cv" onClick={handleDownload}>
+          DOWNLOAD CV
+        </button>
       </div>
       <img
         className="hero-illustration illustration-fading"
