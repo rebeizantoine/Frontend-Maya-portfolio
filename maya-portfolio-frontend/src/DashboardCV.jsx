076cfb3d3c27ed1bf1b73c53a4b573c18c30644d
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-10-05 16:43:01 +0300

    dashboard is done without login feature

--- maya-portfolio-frontend/src/DashboardCV.jsx
+++ maya-portfolio-frontend/src/DashboardCV.jsx
@@ -1,18 +1,69 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function DashboardCV() {
+  const [cvFile, setCVFile] = useState(null);
+  const [cv,setCv]=useState([]);
+  const [doFetch,setDoFetch]=useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/cv');
+        setCv(response.data.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    if (doFetch) {
+      fetchData();
+      setDoFetch(false);
+    }
+  }, [doFetch]);
+  const handleUploadCV = async (e) => {
+    try {
+      if (!cvFile) {
+        console.error('Please select a CV file.');
+        return;
+      }
+  
+      const formData = new FormData();
+      formData.append('image', cvFile);
+  
+      const imgBbResponse = await axios.post(
+        'https://api.imgbb.com/1/upload?key=91d27c7f35f4cd3885f4ada2ac3d2c1c',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+      const newCv = {
+        cv: imgBbResponse.data.data.url,
+      };
+      await axios.put(`http://localhost:5000/api/cv/update/${e.target.name}`, newCv, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      setDoFetch(!doFetch);
+      console.log('CV uploaded to ImgBB and stored in the database:', newCv);
+    } catch (error) {
+      console.error('Error uploading CV and storing in the database:', error);
+    }
+  };
+  
+
   return (
     <fieldset className='cv-fieldset'>
       <div className="cv" id="cv">
         <h1>CV</h1>
         <div className="upload-cv">
+        {cv.length>0?(<img src={cv[0].cv} alt="cv-image" width={50} height={50} className="replace-picture" />):(<div>loading...</div>)}
           <p>Upload your CV (pdf file)</p>
-          <input type="file" accept=".pdf" />
-          <button>Save</button>
+          <input type="file" accept=".pdf" onChange={(e) => setCVFile(e.target.files[0])} />
+          <button name={cv[0]?._id} onClick={handleUploadCV}>Save</button>
         </div>
       </div>
     </fieldset>
   );
 }
-
 export default DashboardCV;
--- maya-portfolio-frontend/src/Login.js
+++ maya-portfolio-frontend/src/Login.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import logo from './logo-maya.png'
+import React, { useState } from 'react';
+import logo from './logo-maya.png';
+import {useNavigate} from "react-router-dom";
 import './login.css'; 
+import axios from 'axios';
 
 function Login() {
+  const [username,setUsername]=useState();
+  const [pass,setPass]=useState();
+  const navigate=useNavigate();
+  const changeUsername=(e)=>{
+    setUsername(e.target.value);
+  }
+  const changePass=(e)=>{
+    setPass(e.target.value);
+  }
+  const handleSubmit=async()=>{
+    try{
+    const result=await axios.post(
+      `http://localhost:5000/api/admin`,
+      JSON.stringify({
+        username:username,
+        password:pass
+      }),
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+    if(!result) throw new Error("Failed while logging in ");
+    if(result){
+        navigate('/dashboard');
+    }
+    }catch(error){
+      console.log(error)
+    }
+  }
   return (
     <div className="login-bg">
       <img src={logo} className="logo-login" alt='logo' />
       <div className="input-login">
         <label>Email</label>
-        <input type="email" id="email" required />
+        <input type="email" id="email" onChange={changeUsername} required />
       </div>
       <div className="input-login">
         <label>Password</label>
-        <input type="password" id="password" required />
+        <input type="password" id="password" onChange={changePass} required />
       </div>
-      <button className="submit-login">SUBMIT</button>
+      <button onClick={handleSubmit} className="submit-login">SUBMIT</button>
     </div>
   );
 }
